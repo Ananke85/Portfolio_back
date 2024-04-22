d0c3c0bb39fbe724ea97006fde5e6c2632dc97e7
Lorena Carmona <126011228+Ananke85@users.noreply.github.com>
2024-04-22 12:54:33 +0200

    changes

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 const app = require("./app");
-const port = process.env.PORT || 3008;
+const port = process.env.PORT || 3007;
 
 app.listen(port, () => {
   console.log(`Server is up and running ⚡ at ${port}`);

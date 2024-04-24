c3b1c10eb9895753958ed4a470e1ca10b5409da2
Lorena Carmona <126011228+Ananke85@users.noreply.github.com>
2024-04-24 12:28:46 +0200

    changes

--- src/mongo/controller/skillController.js
+++ src/mongo/controller/skillController.js
@@ -10,28 +10,9 @@ const getAllSkills = async (req, res) => {
   }
 };
 
-// const postSkill = async (req, res) => {
-//   const { body } = req;
-//   try {
-//     const newSkill = new skillModel(body);
-//     await newSkill.save();
-//     res.status(200).json(newSkill);
-//   } catch (error) {
-//     res.status(500).json({ message: "Sorry, can't post your skill" });
-//   }
-// };
-
 const postSkill = async (req, res) => {
   const { body } = req;
-
   try {
-    const result = await cloudinary.uploader.upload(req.body.image, {
-      folder: "portfolio", // Replace with your desired Cloudinary folder
-      resource_type: "auto", // This will automatically detect the resource type (image, video, raw, etc.)
-    });
-
-    body.image = result.secure_url;
-
     const newSkill = new skillModel(body);
     await newSkill.save();
     res.status(200).json(newSkill);
@@ -40,6 +21,26 @@ const postSkill = async (req, res) => {
     res.status(500).json({ message: "Sorry, can't post your skill" });
   }
 };
+
+// const postSkill = async (req, res) => {
+//   const { body } = req;
+
+//   try {
+//     const result = await cloudinary.uploader.upload(req.body.image, {
+//       folder: "portfolio", // Replace with your desired Cloudinary folder
+//       resource_type: "auto", // This will automatically detect the resource type (image, video, raw, etc.)
+//     });
+
+//     body.image = result.secure_url;
+
+//     const newSkill = new skillModel(body);
+//     await newSkill.save();
+//     res.status(200).json(newSkill);
+//   } catch (error) {
+//     console.error("Error posting skill:", error);
+//     res.status(500).json({ message: "Sorry, can't post your skill" });
+//   }
+// };
 
 module.exports = {
   getAllSkills,
